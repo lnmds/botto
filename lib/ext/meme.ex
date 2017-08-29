eb4fe84ef9cf95241fb694ea5049d993626477db
Luna Mendes <lkmnds@gmail.com>
2017-08-28 22:38:58 -0300

    xkcd: add arg support

--- lib/ext/meme.ex
+++ lib/ext/meme.ex
@@ -11,7 +11,16 @@ defmodule Botto.Memes do
     Cogs.def xkcd() do
       Client.trigger_typing message.channel_id
       raw = HTTPoison.get! "https://xkcd.com/info.0.json"
-      {:ok, comic_data} = Poison.decode(raw.body)
+      {:ok, comic_data} = Poison.decode raw.body
+      Cogs.say comic_data["img"]
+    end
+
+    Cogs.def xkcd(num) do
+      Client.trigger_typing message.channel.id
+
+      raw = HTTPoison.get! "https://xkcd.com/#{num}/info.0.json"
+      {:ok, comic_data} = Poison.decode raw.body
+
       Cogs.say comic_data["img"]
     end
   end
